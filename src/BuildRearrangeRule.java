1a8d14e859b86239441e1bb60c1057938d866763
myboy <myboy@naver.com>
2018-06-26 21:55:06 +0900

    rearrange method 추가

--- src/BuildRearrangeRule.java
+++ src/BuildRearrangeRule.java
@@ -2,4 +2,8 @@
 public abstract class BuildRearrangeRule {
 
 	public abstract void rearrange(BuildOrderQueue queue);
+	
+	public abstract void rearrange(BuildOrderQueue queue, BuildStrategy buildStrategy);
+	
+	public abstract void rearrange(BuildOrderQueue queue, BuildOrderItem buildOrderItem);
 }
